10e30a4dffda195db64059aeb7eb4e67e41cfaa3
vaadikkk <andrukonis2020@gmail.com>
2025-11-07 20:02:18 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+from telegram.ext import Updater, CommandHandler
+import os
+
+# Берем токен из переменных окружения Render
+TOKEN = os.environ.get("8520596263:AAG_XqPj_Kv-mVZcUZ2XT4pEX0AZVfAOk7U")
+
+def start(update, context):
+    update.message.reply_text("Привет! Я работаю 24/7 на Render 🚀")
+
+def help_command(update, context):
+    update.message.reply_text("Напиши /start, чтобы проверить, что я онлайн.")
+
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("help", help_command))
+
+updater.start_polling()
+updater.idle()
